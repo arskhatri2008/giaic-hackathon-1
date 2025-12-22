5255959107cf1f7cd2032e17159cddd6a1994272
Arsalan Ahmed <121305993+arskhatri2008@users.noreply.github.com>
2025-12-22 21:53:38 +0500

    base url update

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -20,10 +20,10 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-username.github.io',
+  url: 'https://giaic-hackathon-1-ten.vercel.app/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'https://giaic-hackathon-1-ten.vercel.app/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
